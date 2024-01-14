f04b9d45917f15fd78f3e7653c970e76af5ecf64
Olivia Batchelor <o.y.batchelor@outlook.com>
2024-01-14 00:37:28 +0000

    footer colour changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,10 @@ function Footer() {
   
   return(
     <div className="fixed bottom-0">
-      <div className="footer flex w-screen items-start   bg-neutral-900">
+      <div className="footer flex w-screen items-start   bg-stone-500">
         <div className="p-5">
-          <h2 className="text-neutral-100 text-sm font-semibold">About Us</h2>
-          <ul className="text-neutral-100 text-sm">
+          <h2 className="text-neutral-700 text-sm font-semibold">About Us</h2>
+          <ul className="text-neutral-600 text-sm">
             <li>Mission Statement </li>
             <li>Our Anti-Wastage Policy </li>
             <li>Terms & Conditions </li>
@@ -13,8 +13,8 @@ function Footer() {
           </ul>
         </div>
         <div className="p-5">
-          <h2 className="text-neutral-100 text-sm font-semibold">Get Inspired</h2>
-          <ul className="text-neutral-100 text-sm">
+          <h2 className="text-neutral-700 text-sm font-semibold">Get Inspired</h2>
+          <ul className="text-neutral-600 text-sm">
             <li>Lookbook </li>
             <li>Natural Inspiration </li>
             <li>Our Creative Process </li>
